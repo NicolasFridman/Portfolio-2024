efba0271481ebabdff07e8e28b68bf4267c19b4e
Nicolas <nicofri2001@gmail.com>
2024-05-31 12:25:30 -0300

    profile picture actualized

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "NICOLAS FRIDMAN",
-  profile: '/profile.jpeg',
+  profile: '/profile2.jpeg',
   designation: "Full Stack Developer",
   description: "My name is NICOLAS FRIDMAN. I am an enthusiastic programmer. I am a quick learner and I am always looking for new ways to improve my skills. I am passionate about exploring new technologies and facing new challenges. I enjoy teamwork; collaborating and learning from others. I am convinced that learning never ends. I have a great interest in Full Stack development and user experience. My main skill is based on JavaScript, and I use it to create intuitive and user-friendly interfaces. If you are looking for an enthusiastic, proactive and willing-to-learn developer, don't hesitate to contact me.",
   email: 'nicofri2001@gmail.com',
